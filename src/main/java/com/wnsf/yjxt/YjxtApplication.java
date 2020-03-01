8f32a4852c8922a312b554a891d828fce6843fbb
zeze <448165315@qq.com>
2020-03-01 22:59:45 +0800

    完成了资源列表，已经添加资源

--- src/main/java/com/wnsf/yjxt/YjxtApplication.java
+++ src/main/java/com/wnsf/yjxt/YjxtApplication.java
@@ -3,9 +3,14 @@ package com.wnsf.yjxt;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
 
 @MapperScan("com.wnsf.yjxt.sys.mapper")
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableCaching(proxyTargetClass = true)//强制使用cglib代理
 public class YjxtApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/wnsf/yjxt/sys/controller/WarningInfoController.java
+++ src/main/java/com/wnsf/yjxt/sys/controller/WarningInfoController.java
@@ -1,19 +1,16 @@
 package com.wnsf.yjxt.sys.controller;
 
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wnsf.yjxt.common.model.R;
-import com.wnsf.yjxt.sys.entity.User;
+import com.wnsf.yjxt.sys.entity.StudentInfo;
 import com.wnsf.yjxt.sys.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Map;
+import java.util.List;
 
 /**
  * <p>
@@ -21,11 +18,12 @@ import java.util.Map;
  * </p>
  *
  * @author 韩坤
- * @since 2020-03-01
+ * @since 2020-02-26
  */
 @Controller
 @RequestMapping("/sys/warning-info")
 public class WarningInfoController {
+
     @Autowired
     private IUserService userService;
 
@@ -36,28 +34,6 @@ public class WarningInfoController {
         return "warning/warning_list_college";
     }
 
-    //获取预警学生列表
-    @GetMapping("data")
-    @ResponseBody
-    public R data(@RequestParam(required = false) Map<String,Object> map, Page<User> page){
-        //1:创建条件构造器
-        QueryWrapper wrapper=new QueryWrapper();
-        //**未知来处的属性,移除
-        map.remove("_");
-        //3:遍历条件集合map
-        for(Map.Entry<String, Object> entry : map.entrySet()){
-            //3.1获取值
-            Object mapValue = entry.getValue();
-            //3.2如果值不为空,拼接字符串
-            if(mapValue!=null&&mapValue!=""){
-                String mapKey = entry.getKey();
-                wrapper.like(mapKey, mapValue);
-            }
 
-        }
-        //调用方法,此方法拥有分页和条件查询功能
-        userService.getStudentInfo(page, wrapper);
-        return R.ok(page);
 
-    }
 }
